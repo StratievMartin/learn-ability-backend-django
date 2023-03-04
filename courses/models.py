4ef458851a1d50ee7432713f40bf0b9ecaff9c04
Martin Stratiev <martinstratiev1@gmail.com>
2023-03-04 21:14:47 +0200

    minor changes

--- courses/models.py
+++ courses/models.py
@@ -4,6 +4,27 @@ from django.db import models
 class Course(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
+    # price
+    # lecturer
+    # lectures
+    # students
+    # keywords
+    # comments
+    # created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title + ", " + self.description
+
+# class Lectures(models.Model):
+#     title = models.CharField(max_length=100)
+#     description = models.CharField(max_length=500)
+#     # price
+#     # lecturer
+#     # lectures
+#     # students
+#     # keywords
+#     # comments
+#     # created = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.title + ", " + self.description
--- courses/settings.py
+++ courses/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "corsheaders",
 ]
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:5173",'http://127.0.0.1:5174']
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:5174"]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
